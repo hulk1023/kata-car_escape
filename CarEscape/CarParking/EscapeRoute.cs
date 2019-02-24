330f587b08274bd7df54ea6deb882b0cabad0aa3
hulk.wang55 <hulk.wang55@TI03BY89>
2019-02-24 23:45:58 +0800

    initial & the 1st red light

---
+++ CarEscape/CarParking/EscapeRoute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarParking
+{
+    public class EscapeRoute
+    {
+        public string[] FindRoute(int[][] carParking)
+        {
+            return new string[]{""};
+        }
+    }
+}
---
+++ CarEscape/CarParkingTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using NUnit.Framework;
+
+using CarParking;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void EscapedFromLv1()
+        {
+            int[][] _carParking = new int[][] {new int[] {2, 0, 0, 0, 0, 0}};
+            var theRoute = new EscapeRoute().FindRoute(_carParking);
+            Assert.AreEqual("R5", theRoute[0]);
+        }
+    }
+}
